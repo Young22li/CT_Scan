c66adbe62828662c9679996e10c7f20eeae72c86
Young22li <121653561+Young22li@users.noreply.github.com>
2024-12-31 15:45:56 +0800

    Add files via upload

---
+++ Line_function/line_functions.h
@@ -0,0 +1,258 @@
+#ifndef LINE_FUNCTIONS_H
+#define LINE_FUNCTIONS_H
+
+#include <iostream>
+using namespace std;
+
+template <size_t n>
+int line1(int x1, int y1, int x2, int y2, int (&P)[n][n])
+{
+    int x, y, dx, dy, dx1, dy1, px, py, xe, ye, i, res = 0;
+    dx = x2 - x1;
+    dy = y2 - y1;
+    dx1 = abs(dx);
+    dy1 = abs(dy);
+    px = 2 * dy1 - dx1;
+    py = 2 * dx1 - dy1;
+    if (dy1 <= dx1)
+    {
+        if (dx >= 0)
+        {
+            x = x1;
+            y = y1;
+            xe = x2;
+        }
+        else
+        {
+            x = x2;
+            y = y2;
+            xe = x1;
+        }
+        res += P[x][y];
+        for (i = 0; x < xe; i++)
+        {
+            x = x + 1;
+            if (px < 0)
+            {
+                px = px + 2 * dy1;
+            }
+            else
+            {
+                if ((dx < 0 && dy < 0) || (dx > 0 && dy > 0))
+                {
+                    y = y + 1;
+                }
+                else
+                {
+                    y = y - 1;
+                }
+                px = px + 2 * (dy1 - dx1);
+            }
+
+            res += P[x][y];
+        }
+    }
+    else
+    {
+        if (dy >= 0)
+        {
+            x = x1;
+            y = y1;
+            ye = y2;
+        }
+        else
+        {
+            x = x2;
+            y = y2;
+            ye = y1;
+        }
+        res += P[x][y];
+        for (i = 0; y < ye; i++)
+        {
+            y = y + 1;
+            if (py <= 0)
+            {
+                py = py + 2 * dx1;
+            }
+            else
+            {
+                if ((dx < 0 && dy < 0) || (dx > 0 && dy > 0))
+                {
+                    x = x + 1;
+                }
+                else
+                {
+                    x = x - 1;
+                }
+                py = py + 2 * (dx1 - dy1);
+            }
+
+            res += P[x][y];
+        }
+    }
+    return res;
+}
+
+
+template <size_t b>
+void line2(int x1, int y1, int x2, int y2, int ray, short (&P)[b][b])
+{
+    int x, y, dx, dy, dx1, dy1, px, py, xe, ye, i;
+    dx = x2 - x1;
+    dy = y2 - y1;
+    dx1 = abs(dx);
+    dy1 = abs(dy);
+    px = 2 * dy1 - dx1;
+    py = 2 * dx1 - dy1;
+    if (dy1 <= dx1)
+    {
+        if (dx >= 0)
+        {
+            x = x1;
+            y = y1;
+            xe = x2;
+        }
+        else
+        {
+            x = x2;
+            y = y2;
+            xe = x1;
+        }
+        P[x][y] += ray;
+        for (i = 0; x < xe; i++)
+        {
+            x = x + 1;
+            if (px < 0)
+            {
+                px = px + 2 * dy1;
+            }
+            else
+            {
+                if ((dx < 0 && dy < 0) || (dx > 0 && dy > 0))
+                {
+                    y = y + 1;
+                }
+                else
+                {
+                    y = y - 1;
+                }
+                px = px + 2 * (dy1 - dx1);
+            }
+
+            P[x][y] += ray;
+        }
+    }
+    else
+    {
+        if (dy >= 0)
+        {
+            x = x1;
+            y = y1;
+            ye = y2;
+        }
+        else
+        {
+            x = x2;
+            y = y2;
+            ye = y1;
+        }
+        P[x][y] += ray;
+        for (i = 0; y < ye; i++)
+        {
+            y = y + 1;
+            if (py <= 0)
+            {
+                py = py + 2 * dx1;
+            }
+            else
+            {
+                if ((dx < 0 && dy < 0) || (dx > 0 && dy > 0))
+                {
+                    x = x + 1;
+                }
+                else
+                {
+                    x = x - 1;
+                }
+                py = py + 2 * (dx1 - dy1);
+            }
+
+            P[x][y] += ray;
+        }
+    }
+}
+
+template <size_t n>
+void line3(int x1, int y1, int x2, int y2, int (&P)[n][n])
+{
+    int x, y, dx, dy, dx1, dy1, px, py, xe, ye, i;
+    dx = x2 - x1;
+    dy = y2 - y1;
+    dx1 = abs(dx);
+    dy1 = abs(dy);
+    px = 2 * dy1 - dx1;
+    py = 2 * dx1 - dy1;
+    if (dy1 <= dx1)
+    {
+        if (dx >= 0)
+        {
+            x = x1;
+            y = y1;
+            xe = x2;
+        }
+        else
+        {
+            x = x2;
+            y = y2;
+            xe = x1;
+        }
+        P[x][y] = 250;
+        for (i = 0; x < xe; i++)
+        {
+            x = x + 1;
+            if (px < 0) px = px + 2 * dy1;
+            else
+            {
+                if ((dx < 0 && dy < 0) || (dx > 0 && dy > 0)) y = y + 1;
+                else y = y - 1;
+            
+                px = px + 2 * (dy1 - dx1);
+            }
+
+            P[x][y] = 250;
+        }
+    }
+    else
+    {
+        if (dy >= 0)
+        {
+            x = x1;
+            y = y1;
+            ye = y2;
+        }
+        else
+        {
+            x = x2;
+            y = y2;
+            ye = y1;
+        }
+        P[x][y] = 250;
+        for (i = 0; y < ye; i++)
+        {
+            y = y + 1;
+            if (py <= 0) py = py + 2 * dx1;
+            else
+            {
+                if ((dx < 0 && dy < 0) || (dx > 0 && dy > 0)) x = x + 1;
+            
+                else x = x - 1;
+                
+                py = py + 2 * (dx1 - dy1);
+            }
+
+            P[x][y] = 250;
+        }
+    }
+}
+
+#endif // LINE_FUNCTIONS_H
